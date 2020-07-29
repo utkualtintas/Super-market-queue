57c8c90bd382c73391046ef1bad5e6ac6752c021
utkualtintas <66638628+utkualtintas@users.noreply.github.com>
2020-07-29 03:52:08 +0300

    Update Cashier.java

--- SuperMarketQueue/SuperMarketQueue/src/Cashier.java
+++ SuperMarketQueue/SuperMarketQueue/src/Cashier.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-/*THIS CODE IS MY OWN WORK. I DID NOT SEARCH FOR SOLUTION, or I DID NOT CONSULT TO ANY  PROGRAM WRITTEN BY OTHER STUDENTS or DID NOT COPY ANY PROGRAM FROM OTHER SOURCES. 
-I READ AND FOLLOWED THE GUIDELINE GIVEN IN THE PROGRAMMING ASSIGNMENT. NAME: UTKU ALTINTAŞ 71479*/
+
 public class Cashier {
 	private static boolean readyToRecieveCustomer;
 	List<Integer> recordOfServiceTimes= new ArrayList<Integer>();
